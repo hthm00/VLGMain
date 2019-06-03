f5f14bc2f32bd24d3470c26ed829f5596be7842d
Minh Huynh <38261965+hthm00@users.noreply.github.com>
2019-06-03 01:07:41 -0700

    First commit

---
+++ [VLG] Master Airway Bill - Sort Remote Customer.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        Update and Download URL Metadata Test
+// @version     1
+// @updateURL   https://gist.github.com/raw/3906612/update-meta-test.meta.js
+// @downloadURL https://github.com/hthm00/VLGMain/raw/master/%5BVLG%5D%20Master%20Airway%20Bill%20-%20Sort%20Remote%20Customer.user.js
+// ==/UserScript==
